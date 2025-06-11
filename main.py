006a6a5afd30c01fa5e80bd45d6726d430506736
Enock Otieno <otienoenock3@gmail.com>
2025-06-11 11:52:07 +0300

    Revert "ask user name"
    This reverts commit 048005ce3459ed1a0f66306e380a7ff3afc9c4cc.

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 # Step 1: Welcome Messages:Greeting and Personality
 
 import time
+import sys
 
 print("Hello, welcome☺, iam Crypto Tom, your side pocket Crypto friend. ")
-print("Ask me about crypto currency, and I can analyze the current crypto trends and give you the promising instruments so that you can trade with a bang 💸💰. ")
+time.sleep(2)
 
 #Step 2:Adding the Crypto dataset
 crypto_data = [
@@ -32,38 +33,39 @@ crypto_data = [
 ]
 
 # Ask User Input
-user_name = input("What is your name: ").upper()
+user_name = input("What is your name: ").title()
 bold_name = f"\033[1m{user_name}\033[0m"
 print(f"Welcome 👌{bold_name}👌, I'm-Crypto Tom!🤖 Its a pleasure to meet you. 🤝")
+time.sleep(3)
+print("Please NOTE💀, Cryptocurrency markets are highly volatile and involve significant risks.\nCrypto Tom is not a financial advisor and will not be liable to risks you incur. ")
+time.sleep(2)
 
 #Loop for continuity
 while True:
 
-    user_input = input("\nWhich crypto coin(s) would you like to dig into?...(Type Exit / quit): ").lower()
-    if user_input in "exit" or user_input in "quit":
+    user_input = input("\nWhich crypto coin would you like to dig into?...(Type Exit / quit): ").strip().lower()
+    if user_input in ["exit","quit"]:
         print("Thank you for visiting Crypto Tom!...A bientot👋 ")
         break
-    else:
-        coins = user_input.split()
 
 # Find Crypto in the data
     found = False
-    for each_coin in coins:
-        for crypto in crypto_data:
-            bold_crypto_name = f"\033[1m{crypto['name']}\033[0m"
-            if crypto["name"].lower() == each_coin.lower():
-                found = True
-                print(f"📊 Analyzing {bold_crypto_name} ......")
-                #time.sleep(7)
+    for crypto in crypto_data:
+        bold_crypto_name = f"\033[1m{crypto['name']}\033[0m"
+        if crypto["name"].lower() == user_input.lower():
+            found = True
+            print(f"📊 Analyzing {bold_crypto_name} ......")
+            time.sleep(5)
 
-                #if else logic
-                if crypto["price_trend"] == "up" and crypto["energy_usage"] != "high" and crypto ["project_viability"] == "strong":
-                    print(f"✅ This is a promising project!! The trend is going, energy usage is low and project viability is strong...good choice to consider ")
-                elif crypto ["price_trend"] == "up" and crypto ["energy_usage"] in ["medium", "high"]:
-                    print(f"Hmmm🤔 the {bold_crypto_name} project is doing ok,BUT please check the energy usage or project viability first")
+            #if else logic
+            if crypto["price_trend"] == "up" and crypto["energy_usage"] != "high" and crypto ["project_viability"] == "strong":
+                print(f"✅ {bold_crypto_name} is a promising project!! The trend is going, energy usage is low and project viability is strong...good choice to consider ")
+            elif crypto ["price_trend"] == "up" and crypto ["energy_usage"] in ["medium", "high"]:
+                print(f"Hmmm🤔 the {bold_crypto_name} project is doing ok,BUT please check the energy usage or project viability first")
 
-                else:
-                    print(f"As a friend, i would not recommend you to take a bite on the {bold_crypto_name}⛔!!⚡project buddie.Its either price is going down or the project is not strong")
+            else:
+                print(f"As a friend, i would not recommend you to take a bite on the {bold_crypto_name}⛔!!⚡project buddie.Its either price is going down or the project is not strong")
 
-        if found:
-            print(f"ooops! buddie, {f"\033[1m{each_coin.upper()}\033[0m"} not found, please try a different one")
+
+    if not found:
+        print("ooops! buddie, i searched and could not find that project, could you please try a different one")'''
